7d4dd8177a8d7aa7a3d9cb023a0d51377f395991
Maksim Lin <maks@manichord.com>
2019-12-09 12:42:14 +1100

    scaffold new package project using stagehand

---
+++ lib/string_res.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library string_res;
+
+export 'src/string_res_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
